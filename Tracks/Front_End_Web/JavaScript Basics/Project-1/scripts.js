588230532c91cf313f0c4f4243d1b1fdfa90fc85
Keith Heffington <lightingcount_@hotmail.com>
2019-12-16 14:37:55 -0600

    Using Variables

--- Tracks/Front_End_Web/JavaScript Basics/Project-1/scripts.js
+++ Tracks/Front_End_Web/JavaScript Basics/Project-1/scripts.js
@@ -1,3 +1,7 @@
 alert("Hello from Treehouse");
 document.write("<h1>Welcome to JavaScript Basics</h1>");
 alert("Thanks for visiting.");
+var message = "Hello!";
+alert(message);
+message = "Welcome to JavaScript Basics";
+alert(message)
