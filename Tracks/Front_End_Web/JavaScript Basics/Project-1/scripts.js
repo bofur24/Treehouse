e9651837e6d26f107afa590c76c22f21b8316878
Keith Heffington <lightingcount_@hotmail.com>
2019-12-16 15:11:15 -0600

    Combining Strings

--- Tracks/Front_End_Web/JavaScript Basics/Project-1/scripts.js
+++ Tracks/Front_End_Web/JavaScript Basics/Project-1/scripts.js
@@ -1,7 +1,7 @@
 alert("Hello from Treehouse");
 document.write("<h1>Welcome to JavaScript Basics</h1>");
-alert("Thanks for visiting.");
-var message = "Hello!";
-alert(message);
-message = "Welcome to JavaScript Basics";
-alert(message)
+var visitorName = prompt("What is your name");
+var message = "Hello " + visitorName;
+message += "! Thanks for comeing to this place of business.";
+console.log(visitorName);
+document.write(message);
