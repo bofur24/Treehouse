e3c142ef1905286406041f42bbaac589fa62ce73
Keith Heffington <lightingcount_@hotmail.com>
2019-12-17 15:53:46 -0600

    Dealing with Numbrs

--- Tracks/Front_End_Web/JavaScript Basics/Project-1/scripts.js
+++ Tracks/Front_End_Web/JavaScript Basics/Project-1/scripts.js
@@ -1,7 +1,20 @@
-alert("Hello from Treehouse");
-document.write("<h1>Welcome to JavaScript Basics</h1>");
-var visitorName = prompt("What is your name");
-var message = "Hello " + visitorName;
-message += "! Thanks for comeing to this place of business.";
-console.log(visitorName);
-document.write(message.toUpperCase());
+// alert("Hello from Treehouse");
+// document.write("<h1>Welcome to JavaScript Basics</h1>");
+// var visitorName = prompt("What is your name");
+// var message = "Hello " + visitorName;
+// message += "! Thanks for comeing to this place of business.";
+// console.log(visitorName);
+// document.write(message.toUpperCase());
+
+var secondsPerMin = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+document.write('<p>There are ' + secondsPerDay + ' seconds in a day</p>');
+
+var HTMLBadges = prompt("How mnay HTML badges do you have?");
+var CSSBadges = prompt('How many CSS badges do you have?');
+var totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
+alert('Wow! you have ' + totalBadges + ' badges!');
