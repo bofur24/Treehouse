fe4f8366d6f260b758f35fd6570fdddd2c843525
Keith Heffington <lightingcount_@hotmail.com>
2020-01-09 14:53:42 -0600

    Solved

--- Tracks/Front_End_Web/Practice/JavaScript Practice/JavaScript If and Else If Statements/js/math.js
+++ Tracks/Front_End_Web/Practice/JavaScript Practice/JavaScript If and Else If Statements/js/math.js
@@ -1,24 +1,39 @@
 // declare program variables
-
+var num1;
+var num2;
+var message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
 // announce the program
 alert("Let's do some math!");
 
 // collect numeric input
-var num1 = prompt("Please type a number");
+num1 = prompt("Please type a number");
 num1 = parseFloat(num1);
-var num2 = prompt("Please type another number");
+num2 = prompt("Please type another number");
 num2 = parseFloat(num2);
 
-// build an HTML message
-var message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
-message += num1 + " + " + num2 + " = " + (num1 + num2);
-message += "<br>";
-message += num1 + " * " + num2 + " = " + (num1 * num2);
-message += "<br>";
-message += num1 + " / " + num2 + " = " + (num1 / num2);
-message += "<br>";
-message += num1 + " - " + num2 + " = " + (num1 - num2);
-  
-// write message to web page
-document.write(message);
+if (num2 === 0) {
+    alert("The second number is 0. You can't divide by zero. Reload and try again.");
+} else if (isNaN(num1) || isNaN(num2)) {
+    alert("At least one of the values you typed is not a number. Reload and try again.")
+}
+
+else {
+    // build an HTML message
+
+    message += num1 + " + " + num2 + " = " + (num1 + num2);
+    message += "<br>";
+    message += num1 + " * " + num2 + " = " + (num1 * num2);
+    message += "<br>";
+    message += num1 + " / " + num2 + " = " + (num1 / num2);
+    message += "<br>";
+    message += num1 + " - " + num2 + " = " + (num1 - num2);
+    
+    // write message to web page
+    document.write(message);
+
+}
+
+
+
+
 
