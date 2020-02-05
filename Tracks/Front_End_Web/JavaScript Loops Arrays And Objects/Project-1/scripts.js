0838cf78c69d81a7e3a03ff4616494f62fc5d4b3
Keith Heffington <lightingcount_@hotmail.com>
2020-02-05 15:03:20 -0600

    Break point

--- Tracks/Front_End_Web/JavaScript Loops Arrays And Objects/Project-1/scripts.js
+++ Tracks/Front_End_Web/JavaScript Loops Arrays And Objects/Project-1/scripts.js
@@ -30,27 +30,30 @@
 
 //do while loop
 
-// var randomNumber = getRandomNumber(10);
-// var guess;
-// var guessCount = 0;
-// var correctGuess = false;
+var randomNumber = getRandomNumber(10);
+var guess;
+var guessCount = 0;
+var correctGuess = false;
 
-// function getRandomNumber( upper ) {
-//   var num = Math.floor(Math.random() * upper) + 1; 
-//   return num;
-// }
+function getRandomNumber( upper ) {
+  var num = Math.floor(Math.random() * upper) + 1; 
+  return num;
+}
 
-// do {
-//   guess = prompt("I am thinking of a number between 1 and 10. What is it?");
-//   guessCount += 1;
-//   if (parseInt(guess) === randomNumber) {
-//     correctGuess = true;
-//   }
-// } while( ! correctGuess) 
-
-// document.write('<h1>You guessed the number!</h1>');
-// document.write('It took you ' + guessCount + ' tries to guess the nubmer ' + randomNumber);
-
+while (guessCount < 10) {
+  guess = prompt("I am thinking of a number between 1 and 10. What is it?");
+  guessCount += 1;
+  if (parseInt(guess) === randomNumber) {
+    correctGuess = true;
+    break;
+  }
+}  
+if  (correctGuess) {
+document.write('<h1>You guessed the number!</h1>');
+document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
+} else {
+  document.write('<h1>Sorry. You did not guess the number.</h1>')
+}
 // For Loop
 
 // var html = ' ';
