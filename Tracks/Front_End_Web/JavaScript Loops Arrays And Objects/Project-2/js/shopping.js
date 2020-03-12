d48487b7d13cdaa08017e7ef4840d99fec822756
Keith Heffington <lightingcount_@hotmail.com>
2020-03-12 15:31:46 -0500

    Created arrys

---
+++ Tracks/Front_End_Web/JavaScript Loops Arrays And Objects/Project-2/js/shopping.js
@@ -0,0 +1,10 @@
+var shoppingList = [ 'carrots', 'milk', 'eggs' ];
+// add item to the end of the array
+shoppingList.push('meat');
+// add item to the begining of the array
+shoppingList.unshift('apple');
+console.log(shoppingList[0]);
+console.log(shoppingList[1]);
+console.log(shoppingList[2]);
+console.log(shoppingList[3]);
+console.log(shoppingList[4]);
