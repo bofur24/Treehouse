f3c69a5682f1bcaccaeb8257e2c755d7e2d4ca3f
Keith Heffington <lightingcount_@hotmail.com>
2020-04-14 10:15:28 -0500

    Cat Food Store

---
+++ Tracks/Beginning_C_Sharp/CatStore/CatStore/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+namespace CatStore
+{
+    
+    class Program
+    {
+        static string Ask(string question)
+        {
+            Console.Write(question);
+            Console.Write(" ");
+            return Console.ReadLine();            
+        }
+
+        static void Wait(int delay)
+        {
+            Console.Write(".");
+            Thread.Sleep(delay);
+            Console.Write(".");
+            Thread.Sleep(delay);
+            Console.Write(".");
+            Console.WriteLine("");
+            Thread.Sleep(delay);            
+        }
+
+        static void Main(string[] args)
+        {
+            // Display a welcome message for the sotre
+            Console.WriteLine("Welcome to the cat food store!");
+
+            // Wait timer before the question is asked
+            Wait(1000);
+
+            // Ask method to get the amount for order from the user
+            string entry = Ask("How many cans are you ordering?");
+            Console.WriteLine("_________________");
+            Console.WriteLine(entry);
+            
+            
+            
+        }
+    }
+}
