fe115f425283fbc210212c8901cd505ad8caf15e
Keith Heffington <lightingcount_@hotmail.com>
2020-04-15 09:27:03 -0500

    TreehouseDefense Set up

---
+++ Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TreehouseDefense
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Tower tower = new Tower();
+            Map map = new Map();
+
+
+            map.Width = 8;
+            map.Height = 5;
+
+            int area = map.Width * map.Height;
+        }
+    }
+}
---
+++ Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/Map.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreehouseDefense
+{
+    class Map
+    {
+        public readonly int Width;
+        public readonly int Height;
+
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
