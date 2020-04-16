d55a724bf69b9c04ca08277ef1d04006965d3ae0
Keith Heffington <lightingcount_@hotmail.com>
2020-04-16 16:02:41 -0500

    Add class MapLocation Test Inheritance

--- Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/Game.cs
+++ Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/Game.cs
@@ -9,9 +9,19 @@ namespace TreehouseDefense
             Tower tower = new Tower();
             Map map = new Map(8, 5);
 
-            Point point = new Point(4, 2);
+            MapLocation x = new MapLocation(4, 2);
 
-           Console.WriteLine(point.DistanceTo(5, 5));
+            Point p = x;
+
+            map.OnMap(new MapLocation(0, 0));
+
+           Console.WriteLine(x.DistanceTo(5, 5));
+
+            Console.WriteLine(x is MapLocation);
+            Console.WriteLine(x is Point);
+
+            Point point = new Point(0, 0);
+            Console.WriteLine(point is MapLocation);
             
         }
     }
---
+++ Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/MapLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreehouseDefense
+{
+    class MapLocation : Point
+    {
+        public MapLocation(int x, int y) : base(x, y)
+        {
+
+        }
+    }
+}
