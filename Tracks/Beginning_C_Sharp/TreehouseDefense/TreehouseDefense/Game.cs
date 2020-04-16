8d154592fd199a33145a2663ad5f3ad0dc7b0465
Keith Heffington <lightingcount_@hotmail.com>
2020-04-16 09:27:37 -0500

    add Method to Point Class

--- Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/Game.cs
+++ Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/Game.cs
@@ -9,7 +9,10 @@ namespace TreehouseDefense
             Tower tower = new Tower();
             Map map = new Map(8, 5);
 
-            int area = map.Width * map.Height;
+            Point point = new Point(4, 2);
+
+           Console.WriteLine(point.DistanceTo(5, 5));
+            
         }
     }
 }
