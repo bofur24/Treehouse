68754c5e967e8df914de6daca0f66cedc8ceb2dd
Keith Heffington <dransiktolkien@yahoo.com>
2020-04-17 07:21:14 -0500

    Using Exceptions, Try and Catch

--- Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/Game.cs
+++ Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/Game.cs
@@ -6,22 +6,37 @@ namespace TreehouseDefense
     {
         static void Main(string[] args)
         {
-            Tower tower = new Tower();
+            //Tower tower = new Tower();
             Map map = new Map(8, 5);
 
-            MapLocation x = new MapLocation(4, 2);
+            try
+            {
+                MapLocation mapLocation = new MapLocation(20, 20, map);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 
-            Point p = x;
 
-            map.OnMap(new MapLocation(0, 0));
 
-           Console.WriteLine(x.DistanceTo(5, 5));
 
-            Console.WriteLine(x is MapLocation);
-            Console.WriteLine(x is Point);
 
-            Point point = new Point(0, 0);
-            Console.WriteLine(point is MapLocation);
+
+           // MapLocation x = new MapLocation(4, 2);
+
+            //Point p = x;
+
+            //map.OnMap(new MapLocation(0, 0));
+
+           //Console.WriteLine(x.DistanceTo(5, 5));
+
+            //Console.WriteLine(x is MapLocation);
+            //Console.WriteLine(x is Point);
+
+            //Point point = new Point(0, 0);
+            //Console.WriteLine(point is MapLocation);
+
             
         }
     }
--- Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/MapLocation.cs
+++ Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/MapLocation.cs
@@ -6,9 +6,12 @@ namespace TreehouseDefense
 {
     class MapLocation : Point
     {
-        public MapLocation(int x, int y) : base(x, y)
+        public MapLocation(int x, int y, Map map) : base(x, y)
         {
-
+            if(!map.OnMap(this))
+            {
+                throw new Exception(x + "," + y + " is outside the boundaries of the map.");
+            }
         }
     }
 }
