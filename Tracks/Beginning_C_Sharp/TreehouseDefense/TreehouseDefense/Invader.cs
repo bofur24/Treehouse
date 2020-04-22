4891b3d9efb52fe60a453bbade7c85e39da4efb9
Keith Heffington <lightingcount_@hotmail.com>
2020-04-22 10:19:31 -0500

    Property Practice

--- Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/Invader.cs
+++ Tracks/Beginning_C_Sharp/TreehouseDefense/TreehouseDefense/Invader.cs
@@ -9,7 +9,15 @@ namespace TreehouseDefense
         private readonly Path _path;
         private int _pathStep = 0;
         public MapLocation Location => _path.GetLocationAt(_pathStep);
- 
+
+        public int Health { get; private set; } = 2; 
+
+        public bool HasScored { get { return _pathStep >= _path.Length; }  }
+
+        public bool IsNeutralized => Health <= 0;
+
+        public bool IsActive => !(IsNeutralized || HasScored);
+
         public Invader(Path  path)
         {
             _path = path;
@@ -18,7 +26,10 @@ namespace TreehouseDefense
 
         public void move() => _pathStep += 1; // syntactic sugar removes the braces from simple methods and properties and makes one-liners.
  
-
+        public void DecreaseHealth(int factor)
+        {
+            Health -= factor;
+        }
 
 
 
